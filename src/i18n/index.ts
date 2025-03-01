ec88c939ab8b32037d45c0ecbc47f55b55adc113
Yury Kurouski <ymorpheus@gmail.com>
2025-03-01 02:53:54 +0100

    fix: add Polish localization and accessibility hint for currency search

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,10 +1,16 @@
 import { getLocales } from "expo-localization";
 import { I18n } from "i18n-js";
 
+import by from "./by.json";
 import en from "./en.json";
+import pl from "./pl.json";
+import ua from "./ua.json";
 
 const i18n = new I18n({
   en: en,
+  by: by,
+  pl: pl,
+  ua: ua,
 });
 
 i18n.locale = getLocales()[0]?.languageCode ?? "en";
